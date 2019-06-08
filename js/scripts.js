d8d11acb2434e2c3f633924c7bea272fe788009f
Oscar Maritim <oscarmaritim3@gmail.com>
2019-06-08 09:35:06 +0300

    added the the defination for design and developement

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
  $(document).ready(function(){
             $('#partOne').hide();
             $('#partTwo').hide();
-            $('partThree').hide();
+            $('#partThree').hide();
 
             $('#design').click(function(){
             	$('#design').hide();
@@ -10,6 +10,23 @@
             $('#partOne').click(function(){
             	$('#partOne').hide();
             	$('#design').show();
+            });
+
+            $('#developement').click(function(){
+            	$('#developement').hide();
+            	$('#partTwo').show();
+            });
+            $('#partTwo').click(function(){
+            	$('#partTwo').hide();
+            	$('#developement').show();
+            });
+             $('#product').click(function(){
+            	$('#product').hide();
+            	$('#partThree').show();
+            });
+            $('#partThree').click(function(){
+            	$('#partThree').hide();
+            	$('#product').show();
             });
 
 
